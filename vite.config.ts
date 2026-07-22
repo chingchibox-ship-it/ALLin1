68576983ac1ca5edc71b8dcaf3ac8de9ff57243a
chingchibox-ship-it <chingchibox@gmail.com>
2026-07-22 15:47:28 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev
 export default defineConfig({
+  base: '/ALLin1/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
